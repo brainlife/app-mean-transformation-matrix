5ee2c7351edb64535a43a7b81aae3e4af2cdfe4d
AuroreBussalb <aurore.bussalb@gmail.com>
2021-03-31 17:00:42 +0200

    update with opt files

--- mean_transformation_matrix.py
+++ mean_transformation_matrix.py
@@ -3,15 +3,19 @@
 import mne
 import numpy as np
 import json
+import os
+import shutil
 
 
 def mean_transformation_matrix(list_raw):
-    """Compute the mean transformation matrix across all runs thanks to head position registered at the beginning of each run.
+    """Compute the mean transformation matrix across all runs thanks to head position registered at the beginning
+    of each run.
 
     Parameters
     ----------
     list_raw: list of instance of mne.io.Raw
-        List of all the runs that we want to realign by computing the mean transformation matrix. At least two runs are required.
+        List of all the runs that we want to realign by computing the mean transformation matrix.
+        At least two runs are required.
 
     Returns
     -------
@@ -55,17 +59,42 @@ def main():
 
     # Read all raw files and store them in a list
     list_raw = []
-    for data_file in config["fif"]:
+    for data_file in config["fif_runs"]:
         raw = mne.io.read_raw_fif(data_file, allow_maxshield=True)
         list_raw.append(raw)
 
-    if len(config["fif"]) < 2:
+    if len(config["fif_runs"]) < 2:
         value_error_message = f'Only one run was given. This App needs at least two runs to compute ' \
                               f'the mean transformation matrix.'
         raise ValueError(value_error_message)
 
+    # Read and save the fif file to be preprocess
+    data_file = (config.pop('fif'))
+    raw = mne.io.read_raw_fif(data_file, allow_maxshield=True)
+    raw.save("out_dir/meg.fif", overwrite=True)
+
+    # Read the crosstalk files
+    cross_talk_file = config.pop('crosstalk')
+    if os.path.exists(cross_talk_file) is True:
+        shutil.copy2(cross_talk_file, 'out_dir/crosstalk_meg.fif')  # required to run a pipeline on BL
+
+    # Read the calibration file
+    calibration_file = config.pop('calibration')
+    if os.path.exists(calibration_file) is True:
+        shutil.copy2(calibration_file, 'out_dir/calibration_meg.dat')  # required to run a pipeline on BL
+
+    # Read the destination file
+    destination_file = config.pop('destination')
+    if os.path.exists(destination_file) is True:
+        shutil.copy2(destination_file, 'out_dir_bad_channels/destination.fif')  # required to run a pipeline on BL
+
+    # Get head pos file
+    head_pos = config.pop('headshape')
+    if os.path.exists(head_pos) is True:
+        shutil.copy2(head_pos, 'out_dir_maxfilter/headshape.pos')  # required to run a pipeline on BL
+
     # Compute mean transformation matrix
-    mean_tm_raw = mean_transformation_matrix(list_raw)
+    mean_transformation_matrix(list_raw)
 
     # Success message in product.json
     dict_json_product['brainlife'].append({'type': 'success',
